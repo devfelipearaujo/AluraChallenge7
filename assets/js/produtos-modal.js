211086ae8b2e366d9e0aba7a4f8197e48fd71d1a
Felipe Araujo <araujo.felipedev@gmail.com>
2023-06-28 11:48:43 -0300

    produtos section modal JS added.

---
+++ assets/js/produtos-modal.js
@@ -0,0 +1,49 @@
+const verMaisBtn = document.querySelectorAll('#produtos__li-btn');
+const produtosModal = document.querySelector('#produtos__modal')
+const produtosModalCloseIcon = document.querySelector('#produtos__modal__icon-close');
+
+/* ----- Opening and closing modal ----- */
+
+verMaisBtn.forEach((arrayElement)=>{
+   arrayElement.addEventListener("click", (evento)=>{
+      produtosModal.showModal();
+   })
+})
+
+produtosModalCloseIcon.addEventListener("click", (evento)=>{
+   produtosModal.close();
+})
+
+
+/* ----- Changing modal product's info ----- */
+
+var modalTitle = document.querySelector('#produtos__modal-title');
+var modalDescription = document.querySelector('#produtos__modal-description');
+var modalPrice = document.querySelector('#produtos__modal-price');
+
+verMaisBtn.forEach((arrayElement)=>{
+   arrayElement.addEventListener("click", (evento)=>{
+      changeProduct(evento.target.parentNode.children[0].textContent, evento.target.parentNode.children[1].textContent, evento.target.parentNode.children[2].textContent)
+   })
+})
+
+function changeProduct(title, description, price) {
+   console.log(title);
+   modalTitle.textContent = title;
+   modalDescription.textContent = description;
+   modalPrice.textContent = price;
+}
+
+/* ----- Preventing default function of form's submit button ----- */
+const produtosForm = document.querySelector('#modal__product__form');
+const produtosModalSubmit = document.querySelector('#modal__product-submit')
+produtosForm.addEventListener('submit', preventDefault);
+
+function preventDefault(event) {
+   event.preventDefault();
+};
+
+/* ----- Setting form's submit button to close the modal ----- */
+produtosModalSubmit.addEventListener("click",(evento)=>{
+   produtosModal.close()
+})
